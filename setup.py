addb2adf4916d36b47aa407f6784e7b242d6f6c0
Manu Joseph <manujosephv@gmail.com>
2021-03-18 12:23:56 +0530

    Bump version: 0.3.0 → 1.0.0-dev0

--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/manujosephv/pytorch_tabular",
-    version="0.3.0",
+    version="1.0.0-dev0",
     zip_safe=False,
 )
