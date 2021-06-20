5bc6f8ed89c935789755d59c0923b875f5f809e3
Manu Joseph <manujosephv@gmail.com>
2021-06-20 09:12:39 +0530

    -- added travis_wait -- re-enabled 3.6

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        # "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
