0544fba3c173c5d2bf5153ef189243ff6e0a186f
Manu Joseph <manujosephv@gmail.com>
2021-09-01 17:10:02 +0530

    Bump version: 0.7.0-dev0 → 0.7.0

--- pytorch_tabular/__init__.py
+++ pytorch_tabular/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Manu Joseph"""
 __email__ = "manujosephv@gmail.com"
-__version__ = "0.7.0-dev0"
+__version__ = "0.7.0"
 
 from . import models
 from .tabular_datamodule import TabularDatamodule
