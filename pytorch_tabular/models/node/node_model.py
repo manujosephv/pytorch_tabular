24080c5048288623bc1cb0264b151d615eac0f57
Luca Actis Grosso <luca.actisgrosso@generali.com>
2021-10-31 08:47:47 +0100

    refactor node_model in order to separate weight backbone form weights head

--- pytorch_tabular/models/node/node_model.py
+++ pytorch_tabular/models/node/node_model.py
@@ -23,10 +23,25 @@ logger = logging.getLogger(__name__)
 class NODEBackbone(pl.LightningModule):
     def __init__(self, config: DictConfig, **kwargs):
         super().__init__()
+        if config.embed_categorical:
+            self.embedding_cat_dim = sum([y for x, y in config.embedding_dims])
         self.save_hyperparameters(config)
         self._build_network()
 
     def _build_network(self):
+        if self.hparams.embed_categorical:
+            self.embedding_layers = nn.ModuleList(
+                [nn.Embedding(x, y) for x, y in self.hparams.embedding_dims]
+            )
+            if self.hparams.embedding_dropout != 0 and self.embedding_cat_dim != 0:
+                self.embedding_dropout = nn.Dropout(self.hparams.embedding_dropout)
+            self.hparams.node_input_dim = (
+                self.hparams.continuous_dim + self.embedding_cat_dim
+            )
+        else:
+            self.hparams.node_input_dim = (
+                self.hparams.continuous_dim + self.hparams.categorical_dim
+            )
         self.dense_block = DenseODSTBlock(
             input_dim=self.hparams.node_input_dim,
             num_trees=self.hparams.num_trees,
@@ -50,57 +65,6 @@ class NODEBackbone(pl.LightningModule):
         self.output_dim = (
             self.hparams.output_dim + self.hparams.additional_tree_output_dim
         )
-
-    def forward(self, x):
-        x = self.dense_block(x)
-        return x
-
-
-class NODEModel(BaseModel):
-    def __init__(self, config: DictConfig, **kwargs):
-        if config.embed_categorical:
-            self.embedding_cat_dim = sum([y for x, y in config.embedding_dims])
-        super().__init__(config, **kwargs)
-
-    def subset(self, x):
-            return x[..., : self.hparams.output_dim].mean(dim=-2)
-
-    def data_aware_initialization(self, datamodule):
-        """Performs data-aware initialization for NODE"""
-        logger.info("Data Aware Initialization....")
-        # Need a big batch to initialize properly
-        alt_loader = datamodule.train_dataloader(batch_size=2000)
-        batch = next(iter(alt_loader))
-        for k, v in batch.items():
-            if isinstance(v, list) and (len(v) == 0):
-                # Skipping empty list
-                continue
-            # batch[k] = v.to("cpu" if self.config.gpu == 0 else "cuda")
-            batch[k] = v.to(self.device)
-
-        # single forward pass to initialize the ODST
-        with torch.no_grad():
-            self(batch)
-
-    def _build_network(self):
-        if self.hparams.embed_categorical:
-            self.embedding_layers = nn.ModuleList(
-                [nn.Embedding(x, y) for x, y in self.hparams.embedding_dims]
-            )
-            if self.hparams.embedding_dropout != 0 and self.embedding_cat_dim != 0:
-                self.embedding_dropout = nn.Dropout(self.hparams.embedding_dropout)
-            self.hparams.node_input_dim = (
-                self.hparams.continuous_dim + self.embedding_cat_dim
-            )
-        else:
-            self.hparams.node_input_dim = (
-                self.hparams.continuous_dim + self.hparams.categorical_dim
-            )
-        self.backbone = NODEBackbone(self.hparams)
-        # average first n channels of every tree, where n is the number of output targets for regression
-        # and number of classes for classification
-
-        self.output_response = utils.Lambda(self.subset)
 
     def unpack_input(self, x: Dict):
         if self.hparams.embed_categorical:
@@ -129,11 +93,47 @@ class NODEModel(BaseModel):
             x = torch.cat(tuple(x), dim=1)
         return x
 
-    def compute_backbone(self, x: Dict):
+    def forward(self, x):
         x = self.unpack_input(x)
         if self.hparams.embed_categorical:
             if self.hparams.embedding_dropout != 0 and self.embedding_cat_dim != 0:
                 x = self.embedding_dropout(x)
+        x = self.dense_block(x)
+        return x
+
+
+class NODEModel(BaseModel):
+    def __init__(self, config: DictConfig, **kwargs):
+        super().__init__(config, **kwargs)
+
+    def subset(self, x):
+            return x[..., : self.hparams.output_dim].mean(dim=-2)
+
+    def data_aware_initialization(self, datamodule):
+        """Performs data-aware initialization for NODE"""
+        logger.info("Data Aware Initialization....")
+        # Need a big batch to initialize properly
+        alt_loader = datamodule.train_dataloader(batch_size=2000)
+        batch = next(iter(alt_loader))
+        for k, v in batch.items():
+            if isinstance(v, list) and (len(v) == 0):
+                # Skipping empty list
+                continue
+            # batch[k] = v.to("cpu" if self.config.gpu == 0 else "cuda")
+            batch[k] = v.to(self.device)
+
+        # single forward pass to initialize the ODST
+        with torch.no_grad():
+            self(batch)
+
+    def _build_network(self):
+        self.backbone = NODEBackbone(self.hparams)
+        # average first n channels of every tree, where n is the number of output targets for regression
+        # and number of classes for classification
+
+        self.output_response = utils.Lambda(self.subset)
+
+    def compute_backbone(self, x: Dict):
         x = self.backbone(x)
         return x
 
