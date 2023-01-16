71d283dff097daa04d0f156a6bd19b1280c8b1af
Manu Joseph <manujosephv@gmail.com>
2023-01-16 12:39:31 +0530

    pre-commit fix

--- src/pytorch_tabular/models/__init__.py
+++ src/pytorch_tabular/models/__init__.py
@@ -4,7 +4,6 @@ from .base_model import BaseModel
 from .category_embedding import CategoryEmbeddingModel, CategoryEmbeddingModelConfig
 from .ft_transformer import FTTransformerConfig, FTTransformerModel
 from .gate import GatedAdditiveTreeEnsembleConfig, GatedAdditiveTreeEnsembleModel
-
 from .mixture_density import MDNConfig, MDNModel
 from .node import NodeConfig, NODEModel
 from .tab_transformer import TabTransformerConfig, TabTransformerModel
