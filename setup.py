6273bde64ab0524de17f79e27833ce3a6dc00e4d
Jirka <jirka.borovec@seznam.cz>
2023-05-10 00:16:52 +0200

    Revert "3.8"
    This reverts commit bbb11adc

--- setup.py
+++ setup.py
@@ -34,13 +34,14 @@ test_requirements = requirements_testing
 setup(
     author="Manu Joseph",
     author_email="manujosephv@gmail.com",
-    python_requires=">=3.8",
+    python_requires=">=3.7",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
