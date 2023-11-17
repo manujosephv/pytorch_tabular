e7d750fa8cb8bc5cb300c64076c721b6a1951c01
Jirka B <j.borovec+github@gmail.com>
2023-11-17 11:46:59 -0500

    1.0.3.dev1

--- setup.py
+++ setup.py
@@ -62,6 +62,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/manujosephv/pytorch_tabular",
-    version="1.0.2",
+    version="1.0.3.dev1",
     zip_safe=False,
 )
