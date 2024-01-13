6ee2b1334bcf6fde322ec7961cfce3fb5113f32c
Programador Artificial <programador.artificial@gmail.com>
2024-01-13 16:45:34 -0300

    Remove wrong changes

--- src/pytorch_tabular/tabular_model_sweep.py
+++ src/pytorch_tabular/tabular_model_sweep.py
@@ -325,16 +325,15 @@ def model_sweep(
                 res_dict["params"] = params
                 results.append(res_dict)
                 if best_model is None:
-                    best_model = copy.deepcopy(tabular_model)
-                    best_score = res_dict[f"test_{rank_metric[0]}"]
+                    best_model = tabular_model
                 else:
                     if is_lower_better:
                         if res_dict[f"test_{rank_metric[0]}"] < best_score:
-                            best_model = copy.deepcopy(tabular_model)
+                            best_model = tabular_model
                             best_score = res_dict[f"test_{rank_metric[0]}"]
                     else:
                         if res_dict[f"test_{rank_metric[0]}"] > best_score:
-                            best_model = copy.deepcopy(tabular_model)
+                            best_model = tabular_model
                             best_score = res_dict[f"test_{rank_metric[0]}"]
     if verbose:
         logger.info("Model Sweep Finished")
